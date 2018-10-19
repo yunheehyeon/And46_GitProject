fc9d3d458399887da0bd8caf646594c018a473d6
yunheehyeon <ok94life@daum.net>
2018-10-19 14:29:24 +0900

    textView 변수 추가

--- app/src/main/java/edu/android/and46_gitproject/MainActivity.java
+++ app/src/main/java/edu/android/and46_gitproject/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
-        textView = setText("안녕하세요");
+        textView.setText("안녕하세요");
     }
 }
