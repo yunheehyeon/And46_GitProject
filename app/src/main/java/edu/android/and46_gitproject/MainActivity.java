51d47e8a2152ac9d2311c1507bd30ec502cb5152
yunheehyeon <ok94life@daum.net>
2018-10-19 14:23:58 +0900

    textView 변수 추가

--- app/src/main/java/edu/android/and46_gitproject/MainActivity.java
+++ app/src/main/java/edu/android/and46_gitproject/MainActivity.java
@@ -2,12 +2,17 @@ package edu.android.and46_gitproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
     }
 }
